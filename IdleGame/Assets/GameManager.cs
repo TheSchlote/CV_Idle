3248e83460b2d2ce75acf79dd5fc7c300cc3326d
TheSchlote <TheSchlote@gmail.com>
2023-03-11 11:14:45 -0600

    build version to my phone

--- IdleGame/Assets/GameManager.cs
+++ IdleGame/Assets/GameManager.cs
@@ -1,6 +1,8 @@
 using Mono.Cecil.Rocks;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,58 +12,55 @@ public class GameManager : MonoBehaviour
     public double money;
     public double dpc;
     public double health;
-    public double healthMax;
+    public double HealthMax
+    {
+        get
+        {
+            return 10 * Math.Pow(2, stage - 1) * BossMultiplyer; ;
+        }
+    }
     public int stage;
     public int stageMax;
     public int kills;
     public int killsMax;
+    public int BossMultiplyer;
+
     public TMP_Text MoneyText;
     public TMP_Text DPCText;
     public TMP_Text StageText;
     public TMP_Text KillsText;
     public TMP_Text healthText;
+    public TMP_Text timerText;
 
     public Button back;
     public Button forward;
 
     public Image heatlhBar;
+    public Image timerBar;
+    public float timer;
+    public float timerCap;
 
     public void Start()
     {
-        dpc = 1;
+        dpc = 100;
         stage = 1;
         stageMax = 1;
         killsMax = 10;
-        healthMax = 10;
-        health = healthMax;
+        BossMultiplyer = 1;
+        health = HealthMax;
     }
 
     public void Update()
     {
         MoneyText.text = $"${money:F2}";
         DPCText.text = $"{dpc} Damage Per Click";
-        StageText.text = $"Stage - {stage}";
         KillsText.text = $"{kills}/{killsMax} Kills";
-        healthText.text = $"{health}/{healthMax} HP";
-        heatlhBar.fillAmount = (float)(health / healthMax);
+        healthText.text = $"{health}/{HealthMax} HP";
+        
+        heatlhBar.fillAmount = (float)(health / HealthMax);
 
-        if(stage > 1)
-        {
-            back.gameObject.SetActive(true);
-        }
-        else
-        {
-            back.gameObject.SetActive(false);
-        }
-
-        if(stage != stageMax)
-        {
-            forward.gameObject.SetActive(true);
-        }
-        else 
-        { 
-            forward.gameObject.SetActive(false); 
-        }
+        back.enabled = stage > 1;
+        forward.enabled = stage != stageMax;
     }
 
     public void Hit()
@@ -69,18 +68,24 @@ public class GameManager : MonoBehaviour
         health -= dpc;
         if(health <= 0)
         {
-            money += 1;
-            health = healthMax;
+            money += Math.Ceiling(HealthMax / 14);
             if (stage == stageMax)
             {
-                kills += 1;
+                kills ++;
                 if (kills >= killsMax)
                 {
                     kills = 0;
-                    stage += 1;
-                    stageMax += 1;
+                    stage++;
+                    stageMax++;
+
+                    bool isBossStage = stage % 5 == 0;
+                    BossMultiplyer = isBossStage ? 10 : 1;
+                    StageText.text = isBossStage ? $"BOSS - {stage}" : $"Stage - {stage}";
+                    killsMax = BossMultiplyer == 10 ? 1 : 10;
                 }
+                
             }
+            health = HealthMax;
         }
     }
 
@@ -88,14 +93,14 @@ public class GameManager : MonoBehaviour
     {
         if( stage > 1)
         {
-            stage -= 1;
+            stage--;
         }
     }
     public void Forward()
     {
         if (stage != stageMax)
         {
-            stage += 1;
+            stage++;
         }
     }
 }
